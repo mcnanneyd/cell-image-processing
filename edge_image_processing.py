25a2b3397f357a0cf40167dc2e1123a59f883bac
Doug McNanney <mcnanney@pop-os.localdomain>
2023-01-14 16:50:31 -0500

    Track intensities of a series of images over time

--- edge_image_processing.py
+++ edge_image_processing.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 import cv2
 import numpy as np
+import os
+from typing import List
+import re
+
+def sort_by_number( string_list: List[str] ) -> List[str]: 
+    """ Sort the """ 
+    convert = lambda text: int(text) if text.isdigit() else text 
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(string_list, key = alphanum_key)
 
 def otsu_edge(image, sigma=0.33):
     # Perform Gaussian blur to reduce noise, you may not need this so much
@@ -21,10 +30,24 @@ def otsu_edge(image, sigma=0.33):
     
     # Edge detected image and threshold array
     return edged, th
+
+
+def get_rois(   brightfield_image: str, lower_a: float = 50, 
+                upper_a: float = 225):
+    """Get ROIs filtered by roi area
+
+    Args:
+        brightfield_image (str): filename of brighfield image
+        lower_a (float, optional): Lower bound for cell area. Defaults to 50.
+        upper_a (float, optional): Upper bound for cell area. Defaults to 225.
+
+    Returns:
+        cells (tuple of numpy arrays containing points that define 
+        the boundaries of each ROI): This is in the exact format that 
+        opencv returns contours 
+    """
     
-def main():
-    filename = 'frame-4500.tif'
-    raw2d = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    raw2d = cv2.imread(brightfield_image, cv2.IMREAD_GRAYSCALE)
     img8 = (raw2d).astype('uint8')
     
     # Get image with ROIs mapped out as nonzero values
@@ -36,17 +59,68 @@ def main():
     # Each numpy array in the tuple contains a list of x-y points that 
     # Make up the boundaries of each cell
     contours, hierarchy = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    larger_contours = []
     
-    # Draw contours over existing raw image
-    # You could draw this on any image you like
-    cv2.drawContours(raw2d, contours, -1, (255,255,255), 1)
+    # Filter rois by their pixel-area
+    for cnt in contours:        
+        area = cv2.contourArea(cnt)
+        if area < lower_a or area > upper_a:
+            continue
+        larger_contours.append(cnt)
+        
+    cells = tuple(larger_contours)
+    print(f"Roi detection complete - found {len(cells)} cells")
+
+    return cells
+    
+def get_intensities(rois: tuple[np.array], image: np.array):
+    intensities = np.zeros(len(rois), dtype=np.float64)
+    # Allocate empty mask
+    mask = np.zeros(image.shape, np.uint8)
+    for i in range(len(rois)):
+        # Clear the mask
+        mask[...] = 0
+        
+        # Draw a single contour (index i) on empty mask
+        cv2.drawContours(mask, rois, i, 255, -1)
+        
+        # Get average intensity in the specified roi
+        intensity = cv2.mean(image, mask)[0]
+        
+        # Update value
+        intensities[i] = intensity
+        
+    return intensities
+
+def get_relative_intensities(rois: tuple[np.array], image_dir: str = '.'):
+    files = sort_by_number([f for f in os.listdir(image_dir) \
+        if (os.path.isfile(f) and f.endswith('.tif'))])
+
+    first_image = cv2.imread(files[0], cv2.IMREAD_GRAYSCALE)
+    print("first image", first_image)
+    initial_intensities = get_intensities(rois, first_image)
+    print(initial_intensities.shape)
     
     
-    # Display
-    cv2.imshow('Raw image w/ Contours Overlayed', raw2d)
-    cv2.waitKey(0)
-    cv2.imshow('edge detection', edge_detected)
-    cv2.waitKey(0)
+    relative_intensities = np.zeros((len(files), initial_intensities.shape[0]))
+    relative_intensities[...] = 1
+    for i, file in enumerate(files[1:]):
+        raw2d = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+        intensities = get_intensities(rois, raw2d)
+        relative_intensities[i+1] = intensities / initial_intensities
+        
+    return files, relative_intensities
+
+def main():
+    brightfield_image = '../StimSplit_01.tif'
+    rois = get_rois(brightfield_image, 120, 125)
+    filenames, intensities = get_relative_intensities(rois)
+    print(intensities)
+    
+    
+    
+    
+    
 
 if __name__=='__main__':
     main()
